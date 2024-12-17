c5f203f9e49aa0678e8bb8ec670171dcc928274e
adentdk <adentrisnadaudkurnia@gmail.com>
2024-12-17 20:51:49 +0700

    update logo

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
       <div className="flex flex-col items-center gap-2">
         <div className="rounded-full shadow-md p-2 overflow-hidden glass bg-neutral-50 bg-opacity-10">
           <Image
-            src="https://firebasestorage.googleapis.com/v0/b/engravethingid.appspot.com/o/assets%2Fcropped-EngraveThing-1-270x270.png?alt=media&token=4a59f11c-176a-4335-9ea8-f74ec778e3f0"
+            src="https://firebasestorage.googleapis.com/v0/b/engravethingid.appspot.com/o/assets%2Flogo.png?alt=media&token=db00e878-9618-44a8-a446-bffd62cda999"
             alt="Engravething ID" width={100} height={100} />
         </div>
         <span className="font-bold text-lg text-white drop-shadow-md shadow-slate-300 text-center">“Kami grafir apapun yang kamu mau”</span>
