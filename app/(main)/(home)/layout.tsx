bfe16d093c37d1f957d83238199c31afde44c8ed
adentdk <adentrisnadaudkurnia@gmail.com>
2024-12-17 20:49:13 +0700

    update logo

--- app/(main)/(home)/layout.tsx
+++ app/(main)/(home)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout(props: {
       <section
         className="hero min-h-screen"
         style={{
-          backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/engravethingid.appspot.com/o/assets%2Fhero-1.webp?alt=media&token=6e05bbb0-5a7a-458c-a68a-402ddbbad492)'
+          backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/engravethingid.appspot.com/o/assets%2Flogo.png?alt=media&token=db00e878-9618-44a8-a446-bffd62cda999)'
         }}>
         <div className="hero-overlay bg-gradient-radial from-transparent to-black backdrop-blur-sm bg-black/50"></div>
         <div className="hero-content flex-col gap-4 w-full">
